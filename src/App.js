c06983bc7e964ad779a5018d669d56974fe030ee
Mattheus Fernandes <mattheus.ale@hotmail.com>
2023-12-20 14:51:53 -0300

    created route for page register and imported the component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Navbar from './component/Navbar/Navbar';
 //PAGES
 import Home from "./pages/Home/Home"
 import About from "./pages/About/About"
+import Register from './pages/Register/Register';
 
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
@@ -17,6 +18,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />}/>
+          <Route path='/register' element={<Register />} />
           <Route path='/about' element={<About />} />
         </Routes>
       </BrowserRouter>
