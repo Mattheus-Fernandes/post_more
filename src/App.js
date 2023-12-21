3896f26160ad3810b891ea5b099fb45f4e14823e
Mattheus Fernandes <mattheus.ale@hotmail.com>
2023-12-21 11:00:42 -0300

    passed the value of user for the context

--- src/App.js
+++ src/App.js
@@ -9,19 +9,46 @@ import Home from "./pages/Home/Home"
 import About from "./pages/About/About"
 import Register from './pages/Register/Register';
 
+//CONTEXT
+import { AuthProvider } from './context/useAuthContext';
+
+//HOOKS
+import { useAuthentication } from './hooks/useAuthentication';
+
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { onAuthStateChanged } from '@firebase/auth';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [user, setUser] = useState(undefined)
+  const {auth} = useAuthentication()
+
+  const loadingUser = user === undefined
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user)
+    })
+  }, [auth])
+
+
+  if(loadingUser){
+    return <h2>Carregando...</h2>
+  }
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/register' element={<Register />} />
-          <Route path='/about' element={<About />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider value={{user}}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/register' element={<Register />} />
+            <Route path='/about' element={<About />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
