00e55b403e6aef0e34aa3f59263c235eda2c84f3
Mattheus Fernandes <mattheus.ale@hotmail.com>
2023-12-21 11:18:02 -0300

    links for user authenticate

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,7 +1,12 @@
 import styles from "./Navbar.module.scss"
 import { NavLink } from 'react-router-dom';
+import {useAuthValue} from "../../context/useAuthContext"
+
 
 const Navbar = () => {
+
+  const {user} = useAuthValue()
+
   return (
     <nav className={styles.navbar}>
       <NavLink to="/">
@@ -12,9 +17,26 @@ const Navbar = () => {
         <li>
           <NavLink to="/">Home</NavLink>
         </li>
-        <li>
-          <NavLink to="/register">Cadastrar</NavLink>
-        </li>
+        {!user && (
+          <>
+            <li>
+            <NavLink to="/login">Login</NavLink>
+          </li>
+          <li>
+            <NavLink to="/register">Cadastrar</NavLink>
+          </li>
+          </>
+        )}
+        {user && (
+          <>
+            <li>
+              <NavLink to="/posts/create">Novo post</NavLink>
+            </li>
+            <li>
+              <NavLink to="/dashboard">Dashboard</NavLink>
+            </li>
+          </>
+        )}
         <li>
           <NavLink to="/about">Sobre</NavLink>
         </li>
