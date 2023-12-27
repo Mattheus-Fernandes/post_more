e231aef809d65b8cbe311fe3c1f24e4075e75490
Mattheus Fernandes <mattheus.ale@hotmail.com>
2023-12-27 14:30:11 -0300

    fixed the database

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,18 +1,19 @@
+
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBGSaLhWlmVKk0aW0c2qdmwJECitT6zd5Y",
-  authDomain: "post-more.firebaseapp.com",
-  projectId: "post-more",
-  storageBucket: "post-more.appspot.com",
-  messagingSenderId: "281269770080",
-  appId: "1:281269770080:web:e88dfea480c9b362797005"
+  apiKey: "AIzaSyC7RwnJoQGzJpUWZamrc6xaxsC2zlB4p-g",
+  authDomain: "blog-7ade7.firebaseapp.com",
+  projectId: "blog-7ade7",
+  storageBucket: "blog-7ade7.appspot.com",
+  messagingSenderId: "396285417609",
+  appId: "1:396285417609:web:b150d79b66b2caaf91ce58"
 };
 
 
 const app = initializeApp(firebaseConfig);
 
-const databae = getFirestore(app)
+const database = getFirestore(app)
 
-export {databae}
+export {database}
