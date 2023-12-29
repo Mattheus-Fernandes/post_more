38f7cc884ddce1dac808b2017e101fd5d849259c
Mattheus Fernandes <mattheus.ale@hotmail.com>
2023-12-29 09:47:46 -0300

    create page About

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+//CSS
+import { Link } from "react-router-dom"
+import styles from "./About.module.scss"
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className={styles.about}>
+      <h1>Sobre o Mini <span>Blog</span></h1>
+      <p>Este projeto consiste em um blog feito com React no front-end e Firesbase no back end</p>
+      <Link to="/posts/create" className="btn">Criar post</Link>
+    </div>
   )
 }
 
